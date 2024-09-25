64cc90a159f3315b9f0b9bc8c60f7ed6cd56d710
yonishiba <>
2024-09-25 15:13:12 +0900

    1st Commit & Push

--- config/index.ts
+++ config/index.ts
@@ -3,9 +3,9 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP',
+  title: 'サンプルチャットボット',
   description: '',
-  copyright: '',
+  copyright: 'VOTRA Co., Ltd.',
   privacy_policy: '',
   default_language: 'en',
 }
