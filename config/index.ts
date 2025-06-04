c927d2f34c7091c5d8c449fc4eb23f561d0faccc
yonishiba <yoshinori.nishibayashi@gmail.com>
2025-06-04 13:21:01 +0900

    ⌨️ CSP設定の変更と、タイトルの変更

--- config/index.ts
+++ config/index.ts
@@ -3,7 +3,7 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Sample Chatbot',
+  title: 'AIが振袖を提案します',
   description: 'AIアシスタントがあなたに合った振袖の提案をします。好きな色や、柄、イメージを教えてください。\n',
   //  + '※このAIチャットは東京都 デジタルサービス局が公開している「都職員のアイデアが詰まった文章生成AI活用事例集」を引用として読み込ませ、その内容から回答するチャットボットです。',
   copyright: 'VOTRA Co., Ltd.',
