1795f8eb19e433d01121492b5fd05077b1805efd
yonishiba <yoshinori.nishibayashi@gmail.com>
2025-06-10 11:56:11 +0900

    🍜 トータル的な調整作業

--- app/components/index.tsx
+++ app/components/index.tsx
@@ -646,7 +646,7 @@ const Main: FC = () => {
 
           {
             hasSetInputs && (
-              <div className='relative grow h-[200px] pc:w-[794px] max-w-full mobile:w-full pb-[66px] mx-auto mb-3.5 overflow-hidden'>
+              <div className='relative grow h-[200px] max-w-full mobile:w-full pb-[66px] mx-auto mb-3.5 overflow-hidden'>{/*pc:w-[794px]*/}
                 <div className='h-full overflow-y-auto' ref={chatListDomRef}>
                   <Chat
                     chatList={chatList}
